53f4b4fad43d24b3216c9f04dcce2a8af8c2ed37
soham kottawar <sohamkottawar2004@gmail.com>
2024-12-01 23:36:54 +0530

    resume url

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
                     <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-orange-400 hover:bg-slate-200 text-white'>
                         <span className='block bg-[121212] hover:bg-slate-800 rounded-full px-5 py-2'>Hire Me</span>
                     </button>
-                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3' >
+                    <button  onClick={() => window.open('https://drive.google.com/file/d/1-mcGDWoWKxAJVts0iUGuQUUpklc8R06C/view?usp=drive_link', '_blank')} className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3' >
                         <span className='block bg-[121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
                     </button>
                 </div>
